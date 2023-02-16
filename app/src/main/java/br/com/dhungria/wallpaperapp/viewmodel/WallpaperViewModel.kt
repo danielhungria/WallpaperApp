99f9105ae13585a1d3e3d60af772b4e558048317
danielhungria <danielhungria14@gmail.com>
2023-02-16 02:11:16 -0300

    create new branch. & feat:download image

--- app/src/main/java/br/com/dhungria/wallpaperapp/viewmodel/WallpaperViewModel.kt
+++ app/src/main/java/br/com/dhungria/wallpaperapp/viewmodel/WallpaperViewModel.kt
@@ -18,6 +18,7 @@ class WallpaperViewModel @Inject constructor(
     private val repository: Repository
 ) : ViewModel() {
 
+    var hideElements: Boolean = true
     private val _wallpaperModel = MutableLiveData<List<WallpaperModel>>()
     val wallpaperModel: LiveData<List<WallpaperModel>>
         get() = _wallpaperModel
