c9ab3fc5fb2497d9626ac05b0379ef91631024f2
danielhungria <danielhungria14@gmail.com>
2023-02-22 02:21:36 -0300

    add admin screen

--- app/src/main/java/br/com/dhungria/wallpaperapp/repository/Repository.kt
+++ app/src/main/java/br/com/dhungria/wallpaperapp/repository/Repository.kt
@@ -6,35 +6,46 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.util.UUID
 import javax.inject.Inject
 
 class Repository @Inject constructor(private val wallpaperDao: WallpaperDao) {
 
     private val firebaseFireStore: FirebaseFirestore = FirebaseFirestore.getInstance()
 
-    fun queryCategory(): Task<QuerySnapshot>{
+    fun queryCategory(): Task<QuerySnapshot> {
         return firebaseFireStore
             .collection("category")
             .orderBy("name", Query.Direction.ASCENDING)
             .get()
     }
-    fun queryWallpaperFiltered(field: String, value: Any): Task<QuerySnapshot>{
+
+    fun queryWallpaperFiltered(field: String, value: Any): Task<QuerySnapshot> {
         return firebaseFireStore
             .collection("wallpaper")
             .whereEqualTo(field, value)
             .get()
     }
 
-    fun queryAllWallpaper():Task<QuerySnapshot>{
+    fun queryAllWallpaper(): Task<QuerySnapshot> {
         return firebaseFireStore
             .collection("wallpaper")
             .get()
     }
 
+    fun uploadWallpaper(wallpaperModel: WallpaperModel, uuid: String) {
+        Firebase.firestore
+            .collection("wallpaper")
+            .document(uuid)
+            .set(wallpaperModel)
+    }
+
     fun getAll() = wallpaperDao.getAll()
     suspend fun insert(wallpaperModel: WallpaperModel) = wallpaperDao.insert(wallpaperModel)
 
-    suspend fun verifyWasFavorite(id: String):Boolean = wallpaperDao.verifyWasFavorite(id)
+    suspend fun verifyWasFavorite(id: String): Boolean = wallpaperDao.verifyWasFavorite(id)
 
     fun getAllFavoritesWallpapers() = wallpaperDao.getAllFavoritesWallpapers()
 
---
+++ app/src/main/java/br/com/dhungria/wallpaperapp/ui/admin/AdminScreenFragment.kt
@@ -0,0 +1,44 @@
+package br.com.dhungria.wallpaperapp.ui.admin
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import br.com.dhungria.wallpaperapp.databinding.AdminScreenFragmentBinding
+import br.com.dhungria.wallpaperapp.viewmodel.AdminViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AdminScreenFragment: Fragment() {
+
+    private lateinit var binding: AdminScreenFragmentBinding
+    private val viewModel: AdminViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = AdminScreenFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonAddToFirebase.setOnClickListener {
+            val name = binding.editTextInputEditTextName.text.toString()
+            val image = binding.editTextInputEditTextImage.text.toString()
+            val category = binding.editTextInputEditTextCategory.text.toString()
+            val popularSwitch = binding.switchButtonPopular.isChecked
+            viewModel.uploadWallpaper(
+                name = name,
+                image = image,
+                category = category,
+                popular = popularSwitch
+            )
+        }
+    }
+
+}
